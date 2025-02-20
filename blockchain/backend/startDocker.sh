b699cadc1b809d75ab06ad4e2a5d917742e5d296
Terry Weatherman <terry.weatherman8112@gmail.com>
2025-02-20 13:51:45 -0800

    Changed gas limit to 50,000,000

--- blockchain/backend/startDocker.sh
+++ blockchain/backend/startDocker.sh
@@ -1,1 +1,1 @@
-sudo docker run -i -p 8545:8545 -p 8546:8546 --rm --name fhevm ghcr.io/zama-ai/ethermint-dev-node:v0.2.4
+sudo docker run -i -p 8545:8545 -p 8546:8546 --rm --name fhevm ghcr.io/zama-ai/ethermint-dev-node:v0.2.4 --rpc.gas-cap 50000000 --rpc.tx-fee-cap 0
